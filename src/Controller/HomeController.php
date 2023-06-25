34d4a8ef904f033023c480f11421950c11c556f1
FRIKAL Ahmed <ahmad.frikal@gmail.com>
2023-06-25 02:15:23 +0100

    template Add

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 class HomeController extends AbstractController
 {
     //home.index c'est le nom de la route;
@@ -10,6 +11,14 @@ class HomeController extends AbstractController
     public function index(): Response
     { 
         return $this->render('home.html.twig');
+    }
+
+    #[Route('/Afficher', name: 'details_products')]
+    public function Afficher(Request $request): Response
+    {
+        dd($request);
+
+        return $this->render('details_products/index.html.twig');
     }
 
     
